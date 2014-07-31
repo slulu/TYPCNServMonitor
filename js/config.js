2867b7b371b50d9e171b677890a8e7659962bbd8
typcn <admin@typcn.com>
2014-07-31 15:34:13 +0800

    del my key

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776309725-9c9cbf53553cdfc7d22f9904',
-	'm776309723-86ee2d0d0992b8d746e58b3f',
-	'm776309726-9d134b679362da8ff24b31ae',
+	'',
 ];
 
 // refresh interval (in seconds)
