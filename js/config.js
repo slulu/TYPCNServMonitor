e8a06550affc7ce18bb03d9cc014edf2112dee75
typcn <admin@typcn.com>
2014-07-31 15:04:43 +0800

    TYPCN Serv Monitor

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm776309725-9c9cbf53553cdfc7d22f9904',
+	'm776309723-86ee2d0d0992b8d746e58b3f',
+	'm776309726-9d134b679362da8ff24b31ae',
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
